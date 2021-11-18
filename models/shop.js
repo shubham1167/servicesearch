11d7cfe4fe48c94a10eb7c5a997615739b68fe13
shubham1167 <82167102+shubham1167@users.noreply.github.com>
2021-11-18 18:05:18 +0530

    Add files via upload

---
+++ models/shop.js
@@ -0,0 +1,90 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Oshop_schema = new Schema
+(
+    {
+        iod:
+        {
+            type:String,
+            required:true
+        },
+        Name:
+        {
+            type:String,
+            required:true
+        },
+        Location:
+        {
+            type:String,
+            required:true
+        },
+        Brand:
+        {
+            type:String,
+            required:true
+        },
+        Shop_info:
+        {
+            type:String,
+            required:true
+        },
+        Estimated_prices:
+        {
+            type:String,
+            required:true
+        }
+    }
+)
+
+// const Linfo = mongoose.Schema
+// ({
+//     Name:
+//     {
+//         type:String,
+//         required:true
+//     }
+// })
+
+// const Lshop_schema = mongoose.Schema(
+//     {
+//         ild:
+//         {
+//             type:String,
+//             required:true
+//         },
+//         Name:
+//         {
+//             type:String,
+//             required:true
+//         },
+//         Location:
+//         {
+//             type:String,
+//             required:true
+//         },
+
+//         Brands:
+//         [{
+//             Name:{
+//                 type:String,
+//                 required:true
+//             }
+//         }],
+
+//         Shop_info:
+//         {
+//             type:String,
+//             required:true
+//         },
+
+//         Estimated_prices:
+//         {
+//             type:String,
+//             required:true
+//         }
+//     }
+// )
+
+const Shop = mongoose.model('official',{Oshop_schema});
+module.exports = Shop;
