c1918be2b41e31383f54cce1e1fac9085708fec3
Shubham Musale <82167102+shubham1167@users.noreply.github.com>
2022-02-04 00:44:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { title } = require('process');
+const { title, off } = require('process');
 const mongoose = require('mongoose');
 const Shop = require('./models/shop'); 
+
 
 const app = express();
 
@@ -11,8 +12,8 @@ app.set('view engine','ejs');
 
 const db = 'mongodb+srv://shubham:shubham123@servicesearch.iugoy.mongodb.net/servicesearch?retryWrites=true&w=majority'
 
-mongoose.connect(db, {useNewUrlParser: true,useUnifiedTopology: true}) //Asychronous task(takes time to execute)
-    .then((result) => app.listen(3000))//Will only accept the request after completion of above connection
+mongoose.connect(db, {useNewUrlParser: true,useUnifiedTopology: true})
+    .then((result) => app.listen(3000))
     .catch((err) => console.log(err)); 
 
 //Static Middleware
@@ -25,8 +26,17 @@ app.get('/',(req,res) =>{
 })
 
 app.get('/area-brand',(req,res) =>{
-    res.render('page2' , {title:'Area-Brand'});
-})
+
+        Shop.find().sort({createdAt: -1})
+        .then((result)=>{
+            res.render('page2', {shops:result})
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+        
+})      
+
 
 app.get('/official-stores',(req,res) =>{
     res.render('page3', {title:'Official Stores'});
